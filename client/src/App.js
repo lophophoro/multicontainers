4ffb17f4b3a883e1c543708912c7b234785c6ada
lophophoro <neo.revolution.punk@gmail.com>
2020-12-15 10:55:40 -0300

    testing deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
-import Fib from './Fib';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
 
 function App() {
   return (
@@ -18,6 +18,14 @@ function App() {
             rel="noopener noreferrer"
           >
             Learn React
+          </a>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Testing deployment
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
